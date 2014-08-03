b7570692a90cea9720e3b951345ede4a618adc92
cobbdb <cobbdb@gmail.com>
2014-08-02 17:52:00 -0700

    try out sl's callback system

--- tasks/saucelabs-jasmine.js
+++ tasks/saucelabs-jasmine.js
@@ -1,3 +1,4 @@
+var request = require('request');
 module.exports = function (grunt) {
     grunt.config.merge({
         'saucelabs-jasmine': {
@@ -11,7 +12,34 @@ module.exports = function (grunt) {
                     build: process.env.TRAVIS_JOB_ID,
                     concurrency: 3,
                     testname: 'jquery-beacons',
-                    browsers: browsers
+                    browsers: browsers,
+                    onTestComplete: function (result, callback) {
+                        var user = process.env.SAUCE_USERNAME;
+                        var pass = process.env.SAUCE_ACCESS_KEY;
+                        request.put({
+                            url: [
+                                'https://saucelabs.com/rest/v1',
+                                user,
+                                'jobs',
+                                result.job_id
+                            ].join('/'),
+                            auth: {
+                                user: user,
+                                pass: pass
+                            },
+                            json: {
+                                passed: !result.passed
+                            }
+                        }, function (error, response, body) {
+                            if (error) {
+                                callback(error);
+                            } else if (response.statusCode !== 200) {
+                                callback(Error('Unexpected response status'));
+                            } else {
+                                callback(null, !result.passed);
+                            }
+                        });
+                    }
                 }
             }
         }
