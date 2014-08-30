78efa5f76cc8b91f702e7400990776006dd6d4a7
cobbdb <cobbdb@gmail.com>
2014-08-29 22:36:38 -0700

    try with just other suite

--- tasks/saucelabs-jasmine.js
+++ tasks/saucelabs-jasmine.js
@@ -3,12 +3,12 @@ module.exports = function (grunt) {
         'saucelabs-jasmine': {
             all: {
                 options: {
+                    testname: 'all',
                     urls: [
                         'http://127.0.0.1:9999/tests/_SpecRunner-global.html',
                         'http://127.0.0.1:9999/tests/_SpecRunner-single.html',
                         'http://127.0.0.1:9999/tests/_SpecRunner-other.html'
-                    ],
-                    testname: 'all',
+                    ]
                 }
             },
             single: {
