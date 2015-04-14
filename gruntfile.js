6bceead7af959e0be67ca45153bc0004fbc9aa31
cobbdb <cobbdb@gmail.com>
2015-04-13 22:55:13 -0700

    Update project deps. [skip ci]

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function (grunt) {
-    // Load in all the external tasks.
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
     grunt.loadTasks('tasks');
 
